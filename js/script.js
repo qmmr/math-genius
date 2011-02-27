90e0549680c9c988cecf075928de3c86dfa3238d
Marcin Kumorek <kumeek@gmail.com>
2011-02-27 19:10:01 +0100

    modified layout, css, js and many other things

--- js/script.js
+++ js/script.js
@@ -1,40 +1,83 @@
+/*
+    Author: Marcin Kumorek
+*/
 $(function(){
-	// $("#jquery-test").html("jQuery is loaded");
-    console.log('jQuery is loaded!');
+    // console.log('yay! jQuery 1.5 is loaded!');
 
-    var $input = $('#results'),
-        $form = $('#results_form'),
-        random_num = getRandomInt(10, 99);
-        $n1 = $('#n1').text(random_num),
-        $n2 = $('#n2').text(get_same(random_num));
+    var $input      = $('#results'),
+        $operation  = $('#operation'),
+        $n1         = $('#num_one'),
+        $n2         = $('#num_two'),
+        $content    = $('#content'),
+        $form       = $('#results_form'),
+        $yresults   = $('#your_results'),
+        operation   = null,
+        eq_num      = 1
+    ;
 
-    function count(n1, n2, val) {
-        if(!isNaN(val)){
-            return (n1 * n2 === val) ? true : false;
-        } else {
-            // return 'you\'ve typed not a number';
-            // return parseFloat(val);
-            return val;
+    /**
+    * function checks if arithmetic operation of selected type is correct
+    * returns boolean value
+    */
+    function count(n1, n2, type, val) {
+        if(!isNaN(val)) {
+            switch(type) {
+                case '+':
+                    return (n1 + n2 === val) ? true : false;
+                    break;
+                case '-':
+                    return (n1 - n2 === val) ? true : false;
+                    break;
+                case '*':
+                    return (n1 * n2 === val) ? true : false;
+                    break;
+                case '/':
+                    return (n1 * n2 === val) ? true : false;
+                    break;
+                default:
+                    return false;
+                    break;
+            }
         }
+        return false;
     };
 
+    /**
+     * when answer is submitted it checks against certain type of arithmetic operation
+     * appends answer to ul#your_results
+     * and picks new operation
+     */
     $form.submit(function(e){
-        var results = Number($input.val());
-        // console.log(count($n1, $n2, results));
-        console.info($n1.text());
-        console.warn($n2.text());
-        console.error(results);
-        if(count($n1.text(), $n2.text(), results)) {
-            console.info('good answer');
-            var rn = getRandomInt(10, 99),
-                rn2 = get_same(rn);
-            $n1.text(rn);
-            $n2.text(rn2);
-            $input.val('');
+        var results = Number($input.val()),
+            n1      = Number($n1.text()),
+            n2      = Number($n2.text()),
+            li      = $('<li></li>')
+        ;
+        
+        if(count(n1, n2, operation, results)) {
+            li.addClass('success')
+                .text(eq_num + ') ' + $n1.text()+ ' ' + operation + ' ' + $n2.text() + ' = ' + results)
+                .hide()
+                .appendTo($yresults)
+                .fadeIn(500)
+            ;
+            pick_numbers('+');
         } else {
-            console.warn('wrong answer');
+            // console.warn('wrong answer');
+            li.addClass('error')
+                .text(eq_num + ') ' + $n1.text()+ ' ' + operation + ' ' + $n2.text() + ' = ' + results)
+                .hide()
+                .appendTo($yresults)
+                .fadeIn(500)
+            ;
+            pick_numbers(operation);
         }
-        // console.log('rn = ' + rn + ' and rn2 = ' + rn2);
+        eq_num ++;
+        if(eq_num > 10) {
+            // alert('that\'s it, you\'re done!');
+            $operation.fadeOut(500).addClass('visuallyhidden');
+            return false;
+        }
         return false;
     });
 
@@ -52,12 +95,39 @@ $(function(){
     function getRandomInt (min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    
+
+    /**
+     * makes new article, assigns id, creates new operation
+     */
+    /* function get_new_operation(t) {
+        var rn  = getRandomInt(10, 99),
+            rn2 = get_same(rn),
+            art = $('<article id="op#'+eq_num+'" class="operation"></article>'),
+            n1  = $('<h4 class="number"></h4>').text(rn),
+            n2  = $('<h4 class="number"></h4>').text(rn2),
+            op  = $('<span></span>').text(t)
+        ;
+        n2.prepend(op);
+        art.append(n1).append(n2);
+        return art;
+    } */
+
     function get_same(num) {
         var remainder = num % 10;
         return (num - (remainder) + (10 - remainder));
     }
 
+    function pick_numbers() {
+        var rn  = getRandomInt(0, 20),
+            rn2 = getRandomInt(0, 20);
+            // rn2 = get_same(rn);
+        $n1.text(rn);
+        $n2.text(rn2);
+        $input.val('');
+    }
+    
+    operation = '+';
+    pick_numbers(operation);
     $input.focus();
 
-});
+});
